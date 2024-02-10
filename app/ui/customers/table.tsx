4272d71172cb03fb1802d2bc5960a106ca1bcca4
Mostafa Qobadi <106018598+MosQobadi@users.noreply.github.com>
2024-02-10 13:49:28 +0330

    saas

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,10 +1,5 @@
-// import Image from 'next/image';
 import { roboto_mono } from '@/app/ui/fonts';
-// import Search from '@/app/ui/search';
-import {
-  // CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
+import { FormattedCustomersTable } from '@/app/lib/definitions';
 
 export default async function CustomersTable({
   customers,
@@ -16,7 +11,6 @@ export default async function CustomersTable({
       <h1 className={`${roboto_mono.className} mb-8 text-xl md:text-2xl`}>
         Customers
       </h1>
-      {/* <Search placeholder="Search customers..." /> */}
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
@@ -31,13 +25,6 @@ export default async function CustomersTable({
                       <div>
                         <div className="mb-2 flex items-center">
                           <div className="flex items-center gap-3">
-                            {/* <Image
-                              src={customer.image_url}
-                              className="rounded-full"
-                              alt={`${customer.name}'s profile picture`}
-                              width={28}
-                              height={28}
-                            /> */}
                             <p>{customer.name}</p>
                           </div>
                         </div>
@@ -88,13 +75,6 @@ export default async function CustomersTable({
                     <tr key={customer.id} className="group">
                       <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
                         <div className="flex items-center gap-3">
-                          {/* <Image
-                            src={customer.image_url}
-                            className="rounded-full"
-                            alt={`${customer.name}'s profile picture`}
-                            width={28}
-                            height={28}
-                          /> */}
                           <p>{customer.name}</p>
                         </div>
                       </td>
